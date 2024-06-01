a799000a697a9a1d58c45a07b20afcef73dcc1f5
echo <play3step@naver.com>
2024-06-01 14:34:52 +0900

    Feat: 현재위치 hook 추가

--- src/components/MainPage/MainContainer.jsx
+++ src/components/MainPage/MainContainer.jsx
@@ -5,19 +5,29 @@ import {
   Marker,
   useNavermaps,
 } from "react-naver-maps";
+import useCurrentLocation from "../../hooks/useCurrentLocation";
 
 const MainContainer = () => {
   const navermaps = useNavermaps();
+  const { location, error } = useCurrentLocation();
+
+  if (!location) {
+    return <Loading>Loading...</Loading>;
+  }
+
+  if (error) {
+    return <ErrorText>Error: {error}</ErrorText>;
+  }
 
   return (
     <MapContainer>
       <MapDiv style={{ width: "100%", height: "100%" }}>
         <NaverMap
-          defaultCenter={new navermaps.LatLng(37.3595704, 127.105399)}
+          defaultCenter={
+            new navermaps.LatLng(location.latitude, location.longitude)
+          }
           defaultZoom={15}
-        >
-          <Marker position={new navermaps.LatLng(37.3595704, 127.105399)} />
-        </NaverMap>
+        ></NaverMap>
       </MapDiv>
     </MapContainer>
   );
@@ -28,4 +38,21 @@ export default MainContainer;
 const MapContainer = styled.div`
   width: 390px;
   height: 844px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const Loading = styled.div`
+  width: 100%;
+  height: 844px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+`;
+
+const ErrorText = styled.div`
+  color: red;
+  text-align: center;
+  margin-top: 20px;
 `;
---
+++ src/hooks/useCurrentLocation.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const useCurrentLocation = () => {
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      setError("Geolocation is not supported by your browser");
+      return;
+    }
+
+    const handleSuccess = (position) => {
+      const { latitude, longitude } = position.coords;
+      setLocation({ latitude, longitude });
+    };
+
+    const handleError = (error) => {
+      setError(error.message);
+    };
+
+    navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+  }, []);
+
+  return { location, error };
+};
+
+export default useCurrentLocation;
